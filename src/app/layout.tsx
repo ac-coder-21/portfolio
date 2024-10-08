1b38a7336bbb3b4fb6713954b577dc71e5c95a68
Amogh Chavan <auuchavan@gmail.com>
2024-10-08 19:55:21 +0530

    Icon Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Amogh Chavan Portfolio</title>
+        <link rel="icon" href="https://img.icons8.com/?size=100&id=43988&format=png&color=000000" type="image/x-con" />
+      </head>
       <body className={inter.className}>
         
         {children}</body>
