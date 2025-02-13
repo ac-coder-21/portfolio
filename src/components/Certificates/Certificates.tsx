13db0301485a5bc35602dbee5d56261c9909053a
Amogh Chavan <auuchavan@gmail.com>
2025-02-13 22:08:30 +0530

    Added Certificates Page

--- src/components/Certificates/Certificates.tsx
+++ src/components/Certificates/Certificates.tsx
@@ -1,10 +1,18 @@
+import React from "react";
+import CertificateLatest from "./CertificateLatest/CertificateLatest";
+import AllCertificates from "./AllCertificates/AllCertificates";
+import MySocials from "../Home/MySocials/MySocials";
+import ContactMe from "../Home/ContactMe/ContactMe";
 
-const Certificates = () => {
+const Certificate = () => {
     return(
         <div>
-            Certificates Page
+            <CertificateLatest />
+            <AllCertificates />
+            <ContactMe />
+            <MySocials />
         </div>
     )
 }
 
-export default Certificates;
+export default Certificate;
